a1da99b9c9c0ee217103013dfc67ae8bebc60ee0
nikita.kulikov <nik.kulik2001@mail.ru>
2022-04-28 21:02:00 +0300

    added eslint to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-function App() {
-    return <div className="App"></div>;
-}
+const App: React.FC = () => (
+  <div className="App">
+    <h1>Hello World</h1>
+  </div>
+);
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-const el = document.getElementById("root");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const el = document.getElementById('root');
 const app = (
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 ReactDOM.render(app, el);
