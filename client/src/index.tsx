de28498689d935c1662aff12c4517051aad0cf51
nikita.kulikov <nik.kulik2001@mail.ru>
2022-04-28 12:11:14 +0300

    added client

---
+++ client/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+const el = document.getElementById("root");
+const app = (
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+ReactDOM.render(app, el);
